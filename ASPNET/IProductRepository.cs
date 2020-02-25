992b25d464e227fba3b0e7e1dc71d175672d31d6
Landon Thurmon <landonthurmon1@gmail.com>
2020-02-25 09:03:41 -0600

    added updateprod

---
+++ ASPNET/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET.Models;
+using System.Data;
+
+namespace ASPNET
+{
+    public interface IProductRepository
+    {
+
+        public IEnumerable<Product> GetAllProducts();
+
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        
+
+        
+    }
+}
---
+++ ASPNET/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET.Models;
+using Dapper;
+
+namespace ASPNET
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return (Product)_conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                    new { id = id });
+
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+
+        /*1-- IProductRepository->> GetProduct(int id)
+         * 2.--ProductRepository-..Implement. GetProduct(int id) dapper
+         * 3.-- ProductController-> viewproduct()
+         * 4.- Create viewproduct.cshtml
+         * 5.- index.cshtml>> add viewproduct trigger
+         */
+    }
+}
